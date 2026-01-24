83f893c8e053b45ad03c10456d771ecd145f6a7a
yousuf-safin <yousufalisafin@gmail.com>
2026-01-24 13:32:25 +0600

    Revert "Feat: (src/app.py) added http response to Item_api app"
    This reverts commit da041bd551837a53f565f536a3b791d2a95e2ad9.

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from models import Item
 app = FastAPI()
 app.add_event_handler("startup", startup_event)
-@app.post("/items/", statuscode=201)
+@app.post("/items/")
 def create_item(item:Item):
     conn = get_db()
     cursor = conn.cursor()
