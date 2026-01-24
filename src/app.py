f056b99da85f688ccd1c5339a6649eaebf97e017
yousuf-safin <yousufalisafin@gmail.com>
2026-01-24 13:43:57 +0600

    Mod: app.py modified to complete run app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
-from database import get_db, startup_event
+from .database import get_db, startup_event
 from fastapi import FastAPI
-from models import Item
+from .models import Item
 app = FastAPI()
 app.add_event_handler("startup", startup_event)
 @app.post("/items/", status_code=201)
